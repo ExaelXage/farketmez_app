934fd2454e12ad61eacd1f0ac64dcce2e4052f64
Gökay Yurt <fgokayyurt@gmail.com>
2026-08-04 21:05:25 +0300

    chore: targetSdk 35'e yükseltildi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
     defaultConfig {
         applicationId = "com.farketmez.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
